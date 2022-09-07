7cf37421a65dea345e3eeb69abe93584ae70c789
jude iwuji <judeiwuji@gmail.com>
2022-09-07 22:45:14 +0100

    Add: Task 12. pstr

--- get_op_func.c
+++ get_op_func.c
@@ -21,6 +21,7 @@ void (*get_op_func(char *opcode))(stack_t **stack, unsigned int line_number)
 		{"mul", op_mul},
 		{"mod", op_mod},
 		{"pchar", op_pchar},
+		{"pstr", op_pstr},
 		{NULL, NULL},
 	};
 	int i;
--- monty.h
+++ monty.h
@@ -69,6 +69,8 @@ void op_div(stack_t **stack, unsigned int line_number __attribute__((unused)));
 void op_mul(stack_t **stack, unsigned int line_number __attribute__((unused)));
 void op_mod(stack_t **stack, unsigned int line_number __attribute__((unused)));
 void op_pchar(stack_t **stack, unsigned int line_number __attribute__((unused)));
+void op_pstr(stack_t **stack, unsigned int line_number
+			 __attribute__((unused)));
 void process_code(stack_t **stack, char *b_code, int lineno);
 void free_parsed(parser_t *head);
 void free_stack(stack_t *head);
